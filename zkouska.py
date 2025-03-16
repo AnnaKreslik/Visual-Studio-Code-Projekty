21cb6701f6e867dbd7d12d6306c4b606ef556d0a
AnnaKreslik <anna.kreslik@gmail.com>
2025-03-16 14:50:08 +0100

    zkouším git poprvé

---
+++ zkouska.py
@@ -0,0 +1,9 @@
+cislo = 5
+if cislo % 3 == 0:
+    print ("Fizz")
+elif cislo % 5 == 0:
+    print ("Buzz")
+elif cislo % 5 == 0 and cislo % 3 == 0:
+    print ("FizzBuzz")
+else:
+    print ("cislo")
